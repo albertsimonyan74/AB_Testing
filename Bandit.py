29f5a6d73ce0be2c4051b958c8b89f7d5b6bb6b5
albertsimonyan74 <albert.simonyan.w@gmail.com>
2025-03-25 21:16:32 +0400

    Adding the Epsilon Greedy class

--- Bandit.py
+++ Bandit.py
@@ -4,8 +4,13 @@
 ############################### LOGGER
 from abc import ABC, abstractmethod
 from loguru import logger
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
-
+BANDIT_REWARDS = [1, 2, 3, 4]
+NUM_TRIALS = 20000
+np.random.seed(42)
 
 class Bandit(ABC):
     ##==== DO NOT REMOVE ANYTHING FROM THIS CLASS ====##
@@ -54,7 +59,42 @@ class Visualization():
 #--------------------------------------#
 
 class EpsilonGreedy(Bandit):
-    pass
+    def __init__(self, p):
+        self.bandits = [{'true_mean': mu, 'mean': 0, 'N': 0} for mu in p]
+        self.epsilon = 1.0
+        self.rewards = []
+        self.regrets = []
+        self.name = "EpsilonGreedy"
+
+    def __repr__(self):
+        return f"EpsilonGreedy with {len(self.bandits)} bandits"
+
+    def pull(self, bandit):
+        return np.random.randn() + bandit['true_mean']
+
+    def update(self, bandit, x):
+        bandit['N'] += 1
+        bandit['mean'] += (x - bandit['mean']) / bandit['N']
+
+    def experiment(self):
+        best_mean = max([b['true_mean'] for b in self.bandits])
+        for t in range(1, NUM_TRIALS + 1):
+            self.epsilon = 1 / t
+            if np.random.random() < self.epsilon:
+                i = np.random.randint(len(self.bandits))
+            else:
+                i = np.argmax([b['mean'] for b in self.bandits])
+
+            x = self.pull(self.bandits[i])
+            self.update(self.bandits[i], x)
+            self.rewards.append((i, x, self.name))
+            self.regrets.append(best_mean - self.bandits[i]['true_mean'])
+
+    def report(self):
+        df = pd.DataFrame(self.rewards, columns=['Bandit', 'Reward', 'Algorithm'])
+        df.to_csv("epsilon_greedy_rewards.csv", index=False)
+        logger.info(f"[EpsilonGreedy] Cumulative Reward: {sum(df['Reward'])}")
+        logger.info(f"[EpsilonGreedy] Cumulative Regret: {sum(self.regrets)}")
 
 #--------------------------------------#
 
